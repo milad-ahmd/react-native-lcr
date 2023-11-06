352cfdd52eec114762c95eea460171f24b5cc4ba
Milad Ahmadi <miladahmadi803@gmail.com>
2023-11-07 02:10:09 +0330

    fix some issues

--- react-native-lcr/ios/Lcr.h
+++ react-native-lcr/ios/Lcr.h
@@ -15,7 +15,15 @@
 // @end
 
 
-#import <Foundation/Foundation.h>
+#ifdef __cplusplus
+#import "react-native-lcr.hpp"
+#endif
+
+#ifdef RCT_NEW_ARCH_ENABLED
+#import "RNLcrSpec.h"
+
+@interface Lcr : NSObject <NativeLcrSpec>
+#else
 #import <React/RCTBridgeModule.h>
 
 @interface Lcr : NSObject <RCTBridgeModule>
